3cf4cbfa2e9f35201b4136a1b7744a7f3ff26609
app <app@apps-MacBook-Pro.local>
2025-01-20 21:31:17 +0300

    initial commit

--- app.py
+++ app.py
@@ -3,19 +3,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy_serializer import serializer
 
-app = Flask (__name__)
-app.config['SQLALCHEMY_DATABESE_URI'] = 'sqlite:///products.db'
-app.config['SQLALCHEMY_TRACK_MODIFICAFTIONS'] = False
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
 class product (db.Model):
     __tablename__ = 'products'
-    id = db.Column(db.Indeger,primary_key = True)
-    name = db.Column(db.String(30),nullabe = False)
-    price = db.Column(db.Indeger,nullalbe = False)
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(30),nullable = False)
+    price = db.Column(db.Integer,nullalble = False)
     category = db.Colunm(db.String(40),nullable = False)
 
 def __repr__(self):
-    return f"<product name:{self.name}, price:{self.price} ,category:{self.category}>"
+    return f"<product name:{self.name} price:{self.price} category:{self.category}>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
